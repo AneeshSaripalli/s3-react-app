809843fdb76708a6ab5c2811172352f703565264
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2021-01-30 15:04:39 -0600

    query lambda for data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import hooray from '../assets/celebrate.png';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import hooray from "../assets/celebrate.png";
 
 export const App = () => {
+  const [text, setText] = useState("");
+  useEffect(function () {
+    axios
+      .get("https://gwsnglh8l8.execute-api.us-east-1.amazonaws.com/dev/hello")
+      .then(({ data }) => setText(data));
+  }, []);
   return (
     <div className="app">
       <img src={hooray} alt="celebrate" />
       <h1>Welcome to my website from an AWS S3!</h1>
+      <h2>{text}</h2>
     </div>
   );
 };
